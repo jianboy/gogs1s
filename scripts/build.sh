3c3f75a2bc532f3c3e74a12b1938d6ce0254d0dc
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2021-08-08 04:32:39 +0800

    修复构建小问题

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 cd "$(dirname "${0}")/.."
 APP_ROOT=$(pwd)
-# export IS_BUILD="true"
+export IS_BUILD="true"
 
 # execute all necessary tasks
 function main() {
 	rm -rf "${APP_ROOT}/dist"
 	cd "${APP_ROOT}/scripts"
-	./build/build-github1s-extensions.sh
+	./build/build-gogs1s-extensions.sh
 	./package.sh
 
 	echo "all build done!"
