7ba1c5520f4e9847f5753620ddb0d318ff2d883d
liuyuqi-dellpc <liuyuqi.gov@msn.cn>
2021-08-20 21:40:12 +0800

    iconv-lite-umd.js 不能使用cdn

---
+++ 1.sh
@@ -0,0 +1,19 @@
+cd D:/liuyuqi/github/git/gogs1s/
+
+cp	--path	vscode-web-gogs1s/src/vs/workbench/browser/parts/activitybar/activitybarPart.ts              bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/welcome/page/browser/welcomePage.ts               bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/files/browser/views/explorerView.ts               bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/browser/workbench.contribution.ts                         bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/browser/parts/activitybar/activitybarActions.ts           bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/browser/parts/titlebar/titlebarPart.ts                    bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/services/themes/common/themeConfiguration.ts              bak
+cp	--path	vscode-web-gogs1s/src/vs/code/browser/workbench/workbench.ts                                 bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/url/browser/trustedDomains.ts                     bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/welcome/page/browser/welcomePage.css              bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/welcome/page/browser/welcomePage.contribution.ts  bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/contrib/welcome/page/browser/vs_code_welcome_page.ts      bak
+cp	--path	vscode-web-gogs1s/src/vs/workbench/browser/parts/activitybar/media/activitybarpart.css       bak
+cp	--path	vscode-web-gogs1s/src/vs/platform/product/common/product.ts                                  bak
+cp	--path	vscode-web-gogs1s/src/vs/gogs1s/notification.ts                                              bak
+cp	--path	vscode-web-gogs1s/src/vs/gogs1s/notification.css                                             bak
+cp	--path	vscode-web-gogs1s/src/vs/gogs1s/util.ts                                                      bak
